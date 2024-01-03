feb14e336873bb9d449e222bf727ba8ced92d371
Jason Kulak <JasonKulak@yahoo.com>
2024-01-03 17:56:51 -0600

    moved models in models folder

---
+++ models/Fruit.js
@@ -0,0 +1,28 @@
+//////////////////
+//Import Dependencies and Connection
+//////////////////
+const mongoose = require("./connection")
+
+////////////////
+// Create Our Fruits Model
+///////////////
+//Destructure Schema and Model into their own variables
+const {Schema, model} = mongoose
+// const Schema = mongoose.Schema
+// const model = mongoose.model
+// ^^^^Same thing as above ^^^^
+
+//Schema - Shape of the Data
+const fruitSchema = new Schema({
+    name: String,
+    color: String,
+    readyToEat: Boolean
+})
+
+//Model - object for interacting with the db
+const Fruit = model("Fruit", fruitSchema)
+
+//////////////////
+//Export the Model
+//////////////////
+module.exports = Fruit
--- server.js
+++ server.js
@@ -6,40 +6,8 @@ const express = require("express") // our web framework
 const morgan = require("morgan") // our logger
 const methodOverride = require("method-override") // override forms
 const mongoose = require("mongoose") // connect to our mongoosedb
+const Fruit = require("./models/Fruit")
 
-/////////////////////
-//DATABASE Connection
-/////////////////////
-//Our database connection string
-const DATABASE_URL = process.env.DATABASE_URL
-
-//establish our connection
-mongoose.connect(DATABASE_URL)
-
-//Events for when the connection changes
-mongoose.connection
-.on("open", () => {console.log("connected to Mongo")})
-.on("close", () => {console.log("Disconnected to Mongo")})
-.on("error", (error) => {console.log(error)})
-
-////////////////
-// Create Our Fruits Model
-///////////////
-//Destructure Schema and Model into their own variables
-const {Schema, model} = mongoose
-// const Schema = mongoose.Schema
-// const model = mongoose.model
-// ^^^^Same thing as above ^^^^
-
-//Schema - Shape of the Data
-const fruitSchema = new Schema({
-    name: String,
-    color: String,
-    readyToEat: Boolean
-})
-
-//Model - object for interacting with the db
-const Fruit = model("Fruit", fruitSchema)
 
 /////////////////
 //Express App Object
