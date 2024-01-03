02db10932f95896d190f15fa7a072f09d711e9f6
Jason Kulak <JasonKulak@yahoo.com>
2024-01-02 18:27:41 -0600

    made schema

--- server.js
+++ server.js
@@ -21,3 +21,22 @@ mongoose.connection
 .on("open", () => {console.log("connected to Mongo")})
 .on("close", () => {console.log("Disconnected to Mongo")})
 .on("error", (error) => {console.log(error)})
+
+////////////////
+// Create Our Fruits Model
+///////////////
+//Destructure Schema and Model into their own variables
+const {Schema, model} = mongoose
+// const Schema = mongoose.Schema
+// const model = mongoose.model
+// ^^^^Same thing as above ^^^^
+
+//Schema - Shape of the Data
+const fruitSchema = new Schema({
+    name: String,
+    color: String,
+    readyToEat: Boolean
+})
+
+//Model - object for interacting with the db
+const Fruit = model("Fruit", fruitSchema)
